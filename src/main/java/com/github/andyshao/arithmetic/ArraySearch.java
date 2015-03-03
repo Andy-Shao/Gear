9b65a5a5d7ae8bfd87e0cbdffa8471a5fbfb649a
Andy-Shao <hj13035952619@126.com>
2015-03-04 00:03:22 +0800

    add ArraySearch

---
+++ src/main/java/com/github/andyshao/arithmetic/ArraySearch.java
@@ -0,0 +1,54 @@
+package com.github.andyshao.arithmetic;
+
+import java.lang.reflect.Array;
+import java.util.Comparator;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) Mar 3, 2015<br>
+ * Encoding:UNIX UTF-8
+ * 
+ * @author Andy.Shao
+ *
+ */
+public final class ArraySearch {
+    private ArraySearch() {
+        throw new AssertionError("No com.github.andyshao.arithmetic.ArraySearch instances for you!");
+    }
+
+    /**
+     * binary search(二分查找)
+     * 
+     * @param sorted sorted array
+     * @param target the data which want to be found out
+     * @param comparator {@link Comparator}
+     * @param <ARRAY> array type
+     * @param <DATA> data type
+     * @return if can't find out then return -1
+     */
+    @SuppressWarnings("unchecked")
+    public static final <ARRAY, DATA> int bisearch(ARRAY sorted , DATA target , Comparator<DATA> comparator) {
+        if(!sorted.getClass().isArray()) throw new IllegalArgumentException(sorted + " is not a array.");
+        
+        //Continue searching until the left and right indices cross.
+        int left = 0;
+        int right = Array.getLength(sorted) - 1;
+        
+        while(left <= right){
+            int middle = (left + right) / 2;
+            
+            int compare = comparator.compare((DATA) Array.get(sorted , middle) , target);
+            //Prepare to search to the right of the middle index.
+            if(compare < 0) left = middle + 1;
+            //Prepare to search to the left of the middle index.
+            else if(compare > 0) right = middle - 1;
+            //Return the exact index where the data has been found.
+            else return middle;
+        }
+        
+        //Return that the data was not found.
+        return -1;
+    }
+}
---
+++ src/test/java/com/github/andyshao/arithmetic/ArraySearchTest.java
@@ -0,0 +1,25 @@
+package com.github.andyshao.arithmetic;
+
+import java.util.Comparator;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArraySearchTest {
+    private final Comparator<Integer> comparator = (i1 , i2) -> {
+        return Integer.compare(i1 , i2);
+    };
+    private final int[] data = new int[] {
+        0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9
+    };
+
+    @Test
+    public void testBisearch() {
+        int index = ArraySearch.bisearch(this.data , 3 , this.comparator);
+        Assert.assertThat(index , Matchers.is(3));
+
+        index = ArraySearch.bisearch(this.data , 15 , this.comparator);
+        Assert.assertThat(index , Matchers.is(-1));
+    }
+}
