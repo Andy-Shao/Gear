7b63f23ee01497ec1a42ae0e3bb6ff0d1c3f4c24
Andy-Shao <hj13035952619@126.com>
2015-09-01 08:03:29 +0800

    rename test class name

--- src/test/java/com/github/andyshao/nio/ByteBufferOperationTest.java
+++ src/test/java/com/github/andyshao/nio/ByteBufferOperationTest.java
@@ -7,7 +7,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ByteBuffereOperationTest {
+public class ByteBufferOperationTest {
     private volatile ByteBuffer buffer;
 
     @Before
