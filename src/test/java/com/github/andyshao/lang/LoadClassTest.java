bf4d329947042b591ecda4e69930d2843ffc61bc
Andy-Shao <hj13035952619@126.com>
2015-09-07 07:48:25 +0800

    add the demo of LoadClass

--- src/test/java/com/github/andyshao/lang/LoadClassTest.java
+++ src/test/java/com/github/andyshao/lang/LoadClassTest.java
@@ -1,31 +1,51 @@
 package com.github.andyshao.lang;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import com.github.andyshao.nio.BufferReader;
 import com.github.andyshao.nio.ByteBufferReader;
 
 public class LoadClassTest {
-    private static final String CLASS_PATH = "com/github/andyshao/lang/ClassDemo.java";
+    static class MyClassLoader extends ClassLoader {
+        private final String basePath;
 
-    @Before
-    public void before() throws IOException {
-        try (
-            InputStream inputStream =
-                Thread.currentThread().getContextClassLoader().getResourceAsStream(LoadClassTest.CLASS_PATH);
-            ReadableByteChannel channel = Channels.newChannel(inputStream);
-            ByteBufferReader reader = new ByteBufferReader(channel);) {
-            while (reader.hasNext())
-                System.out.println(new String(reader.read()));
+        public MyClassLoader(String basePath) {
+            this.basePath = basePath;
+        }
+
+        public MyClassLoader(String basePath , ClassLoader parent) {
+            super(parent);
+            this.basePath = basePath;
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            byte[] data = this.loadClassData(name);
+            return this.defineClass(name , data , 0 , data.length);
+        }
+
+        private byte[] loadClassData(String name) {
+            try (
+                ByteBufferReader reader =
+                    new ByteBufferReader(Channels.newChannel(new FileInputStream(this.basePath
+                        + name.replace("." , "//") + ".class")))) {
+                reader.setFindSeparatePoint((buffer) -> new BufferReader.SeparatePoint(-1));
+                return reader.read();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     @Test
     public void loadTest() {
+        //        MyClassLoader cl = new MyClassLoader("myClassLoader");  
+        //        Class<?> clazz = cl.loadClass("classloader.Animal");  
+        //        Animal animal=(Animal) clazz.newInstance();  
+        //        animal.say();  
     }
 }
