d39dae86472898859eae0bb1c5fc67572ca5f447
Andy-Shao <hj13035952619@126.com>
2015-09-07 12:41:15 +0800

    finishing the ClassAssembly

--- src/main/java/com/github/andyshao/lang/ClassAssembly.java
+++ src/main/java/com/github/andyshao/lang/ClassAssembly.java
@@ -6,12 +6,32 @@ package com.github.andyshao.lang;
  * Descript:<br>
  * Copyright: Copryright(c) Sep 7, 2015<br>
  * Encoding:UNIX UTF-8
+ * 
  * @author Andy.Shao
- *
- * @param <E> object type
  */
 @FunctionalInterface
-public interface ClassAssembly<E> {
+public interface ClassAssembly {
+    public static final ClassAssembly DEFAULT = new ClassAssembly() {
 
-    Class<E> assemble(String name, byte[] bs);
+        @SuppressWarnings("unchecked")
+        @Override
+        public <E> Class<E> assemble(String name , byte[] bs) {
+            final ClassLoader classLoader = new ClassLoader() {
+
+                @Override
+                protected Class<?> findClass(String name) throws ClassNotFoundException {
+                    return this.defineClass(name , bs , 0 , bs.length);
+                }
+
+            };
+
+            try {
+                return (Class<E>) classLoader.loadClass(name);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
+
+    <E> Class<E> assemble(String name , byte[] bs);
 }
