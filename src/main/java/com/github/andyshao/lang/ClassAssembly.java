aeaa25680943e92348e92582000dc52bca16fa74
Andy-Shao <hj13035952619@126.com>
2015-09-07 08:10:10 +0800

    create ClassAssembly

---
+++ src/main/java/com/github/andyshao/lang/ClassAssembly.java
@@ -0,0 +1,17 @@
+package com.github.andyshao.lang;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) Sep 7, 2015<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ * @param <E> object type
+ */
+@FunctionalInterface
+public interface ClassAssembly<E> {
+
+    Class<E> assemble(byte[] bs) throws ClassNotFoundException;
+}
