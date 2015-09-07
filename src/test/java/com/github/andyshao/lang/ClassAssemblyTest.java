651cadacd3293fd7d004d9dcfaca205770851115
Andy-Shao <hj13035952619@126.com>
2015-09-08 06:55:52 +0800

    ClassAssemblyTest

--- src/test/java/com/github/andyshao/lang/ClassAssemblyTest.java
+++ src/test/java/com/github/andyshao/lang/ClassAssemblyTest.java
@@ -1,6 +1,5 @@
 package com.github.andyshao.lang;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.channels.Channels;
@@ -15,37 +14,6 @@ import com.github.andyshao.nio.ByteBufferReader;
 import com.github.andyshao.reflect.Reflects;
 
 public class ClassAssemblyTest {
-    static class MyClassLoader extends ClassLoader {
-        private final String basePath;
-
-        public MyClassLoader(String basePath) {
-            this.basePath = basePath;
-        }
-
-        public MyClassLoader(String basePath , ClassLoader parent) {
-            super(parent);
-            this.basePath = basePath;
-        }
-
-        @Override
-        protected Class<?> findClass(String name) throws ClassNotFoundException {
-            byte[] data = this.loadClassData(name);
-            return this.defineClass(name , data , 0 , data.length);
-        }
-
-        private byte[] loadClassData(String name) {
-            try (
-                ByteBufferReader reader =
-                    new ByteBufferReader(Channels.newChannel(new FileInputStream(this.basePath
-                        + name.replace("." , "//") + ".class")))) {
-                reader.setFindSeparatePoint((buffer) -> new BufferReader.SeparatePoint(-1));
-                return reader.read();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
     private static final String CLASS_PATH = "com/github/andyshao/lang/ClassDemo.class";
 
     @Test
