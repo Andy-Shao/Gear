6ee64af8e3212cf4dada89ab29981678355dd9f6
Andy-Shao <hj13035952619@126.com>
2015-09-10 11:09:53 +0800

    format class

--- src/main/java/com/github/andyshao/system/Main.java
+++ src/main/java/com/github/andyshao/system/Main.java
@@ -6,17 +6,18 @@ package com.github.andyshao.system;
  * Descript:<br>
  * Copyright: Copryright(c) Sep 10, 2015<br>
  * Encoding:UNIX UTF-8
+ * 
  * @author Andy.Shao
  *
  */
 public class Main {
 
-    public static void main(String[] args) {
-        Task myTask = buildTask();
-        myTask.run(0 , args);
-    }
-
     private static Task buildTask() {
         return new NoArgumentTask(new SystemPropertiesTask());
+    }
+
+    public static void main(String[] args) {
+        Task myTask = Main.buildTask();
+        myTask.run(0 , args);
     }
 }
--- src/main/java/com/github/andyshao/system/SystemPropertiesTask.java
+++ src/main/java/com/github/andyshao/system/SystemPropertiesTask.java
@@ -8,25 +8,26 @@ import java.util.Properties;
  * Descript:<br>
  * Copyright: Copryright(c) Sep 10, 2015<br>
  * Encoding:UNIX UTF-8
+ * 
  * @author Andy.Shao
  *
  */
 public class SystemPropertiesTask implements Task {
     public static final String KEY_WORDS = "-sp";
     public final Task nextTask;
-    
+
     public SystemPropertiesTask() {
         this(Task.EMTPY_TASK);
     }
-    
+
     public SystemPropertiesTask(Task nextTask) {
         this.nextTask = nextTask;
     }
 
     @Override
     public boolean isDuty(int index , String[] args) {
-        if(args == null || args.length == 0) return false;
-        else if(args[0].endsWith(KEY_WORDS)) return true;
+        if (args == null || args.length == 0) return false;
+        else if (args[0].endsWith(SystemPropertiesTask.KEY_WORDS)) return true;
         return false;
     }
 
@@ -34,13 +35,13 @@ public class SystemPropertiesTask implements Task {
     public Task nextTask() {
         return this.nextTask;
     }
-    
+
     @Override
     public void process(int index , String[] args) {
         if (args.length == 1) {
             Properties properties = System.getProperties();
             for (Object key : properties.keySet())
-                System.out.println(key + " = " +properties.get(key));
+                System.out.println(key + " = " + properties.get(key));
         } else System.out.println(System.getProperty(args[1]));
     }
 
