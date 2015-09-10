8191dc5ef688ecf2e792d1044c6fc9fb5eb1832f
Andy-Shao <hj13035952619@126.com>
2015-09-10 11:24:46 +0800

    add JvmTask

---
+++ src/main/java/com/github/andyshao/system/JvmTask.java
@@ -0,0 +1,33 @@
+package com.github.andyshao.system;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) Sep 10, 2015<br>
+ * Encoding:UNIX UTF-8
+ * 
+ * @author Andy.Shao
+ *
+ */
+public class JvmTask implements Task {
+
+    @Override
+    public boolean isDuty(String[] args) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Task nextTask() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void process(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
