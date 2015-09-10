f45364854911f88083c4d60bb6fa33a83449f2cc
Andy-Shao <hj13035952619@126.com>
2015-09-10 11:31:40 +0800

    add VersionTask

---
+++ src/main/java/com/github/andyshao/system/VersionTask.java
@@ -0,0 +1,34 @@
+package com.github.andyshao.system;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) Sep 10, 2015<br>
+ * Encoding:UNIX UTF-8
+ * 
+ * @author Andy.Shao
+ *
+ */
+public class VersionTask implements Task {
+    public static final String KEY_WORDS = "-version";
+
+    @Override
+    public boolean isDuty(String[] args) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Task nextTask() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void process(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
