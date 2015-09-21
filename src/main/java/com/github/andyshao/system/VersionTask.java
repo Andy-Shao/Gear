a5c83539ae07ca6b2160d0fbcf5f7d8063414a08
Andy-Shao <hj13035952619@126.com>
2015-09-21 12:08:33 +0800

    finishing VersionTask

--- src/main/java/com/github/andyshao/system/VersionTask.java
+++ src/main/java/com/github/andyshao/system/VersionTask.java
@@ -1,5 +1,20 @@
 package com.github.andyshao.system;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.github.andyshao.lang.GeneralSystemProperty;
+
 /**
  * 
  * Title:<br>
@@ -12,6 +27,7 @@ package com.github.andyshao.system;
  */
 public class VersionTask implements Task {
     public static final String KEY_WORDS = "-version";
+    private static final String TARGET = "META-INF/maven/com.github.Andy-Shao/Gear/pom.xml";
     private volatile Task nextTask = Task.EMTPY_TASK;
 
     @Override
@@ -27,8 +43,22 @@ public class VersionTask implements Task {
 
     @Override
     public void process(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println(this.getClass());
+        try (final ZipFile zipFile = new ZipFile(new File(GeneralSystemProperty.JAVA_CLASS_PATH.value()))) {
+            final ZipEntry zipEntry = zipFile.getEntry(VersionTask.TARGET);
+            final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder db = factory.newDocumentBuilder();
+            final Document xmlDoc = db.parse(zipFile.getInputStream(zipEntry));
+            final Element root = xmlDoc.getDocumentElement();
+            
+            System.out.println("groupId: " + root.getElementsByTagName("groupId").item(0).getTextContent());
+            System.out.println("artifactId: " + root.getElementsByTagName("artifactId").item(0).getTextContent());
+            System.out.println("version: " + root.getElementsByTagName("version").item(0).getTextContent());
+            System.out.println("packaging: " + root.getElementsByTagName("packaging").item(0).getTextContent());
+            System.out.println("description: " + root.getElementsByTagName("description").item(0).getTextContent());
+            System.out.println("url: " + root.getElementsByTagName("url").item(0).getTextContent());
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void setNextTask(Task nextTask) {
