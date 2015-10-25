60239cf72a6f73d812a69e7eb0dcc01fbb959b34
Andy-Shao <hj13035952619@126.com>
2015-10-25 16:13:02 +0800

    add javadoc in IntBufferOperation

--- src/main/java/com/github/andyshao/nio/IntBufferOperation.java
+++ src/main/java/com/github/andyshao/nio/IntBufferOperation.java
@@ -13,6 +13,15 @@ import java.nio.IntBuffer;
  *
  */
 public final class IntBufferOperation {
+    /**
+     * Get data by position
+     * 
+     * @param buffer the {@link IntBuffer} which should be extracted
+     * @param start the start position of {@link IntBuffer} which should be
+     *            extracted
+     * @param length the length of {@link IntBuffer} which should be extracted
+     * @return the data you want to
+     */
     public static int[] getInts(IntBuffer buffer , int start , int length) {
         final IntBuffer tmp = buffer.asReadOnlyBuffer();
         tmp.position(start);
@@ -20,6 +29,17 @@ public final class IntBufferOperation {
         return IntBufferOperation.usedArray(tmp);
     }
 
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from head to end.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link IntBuffer} which should be searched
+     * @param start the start position of {@link IntBuffer}
+     * @param length the length of {@link IntBuffer}
+     * @param is the data which should be found for
+     * @return if can't find out then return -1
+     */
     public static int indexOf(IntBuffer buffer , int start , int length , int... is) {
         IntBuffer tmp = buffer.asReadOnlyBuffer();
         tmp.position(start);
@@ -27,6 +47,16 @@ public final class IntBufferOperation {
         return IntBufferOperation.indexOf(tmp , is);
     }
 
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from head to end.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link IntBuffer} which should be searched
+     * @param is the data which should be found out
+     * @return if can't find out then return -1
+     * @see IntBufferOperation#indexOf(IntBuffer, int, int, int...)
+     */
     public static int indexOf(IntBuffer buffer , int[] is) {
         if (is.length == 0) throw new IllegalArgumentException("is is empty");
         if (is.length > buffer.limit() - buffer.position()) return -1;
@@ -50,7 +80,35 @@ public final class IntBufferOperation {
         return position;
     }
 
-    public static int lastIndexOf(IntBuffer buffer , int... is) {
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from head to end.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link IntBuffer} which should be searched
+     * @param start the start position of {@link IntBuffer}
+     * @param length the length of {@link IntBuffer}
+     * @param is the data which should be found out
+     * @return if can't find out then return -1
+     */
+    public static int lastIndexOf(IntBuffer buffer , int start , int length , int... is) {
+        IntBuffer tmp = buffer.asReadOnlyBuffer();
+        tmp.position(start);
+        tmp.limit(start + length);
+        return IntBufferOperation.lastIndexOf(tmp , is);
+    }
+
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from head to end.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link IntBuffer} which should be searched
+     * @param is the data which should be found out
+     * @return if can't find out then return -1
+     * @see IntBufferOperation#lastIndexOf(IntBuffer, int, int, int...)
+     */
+    public static int lastIndexOf(IntBuffer buffer , int[] is) {
         if (is.length == 0) throw new IllegalArgumentException("is is empty");
         if (is.length > buffer.limit() - buffer.position()) return -1;
         final IntBuffer tmpByteBuffer = buffer.asReadOnlyBuffer();
@@ -77,13 +135,14 @@ public final class IntBufferOperation {
         return position;
     }
 
-    public static int lastIndexOf(IntBuffer buffer , int start , int length , int... is) {
-        IntBuffer tmp = buffer.asReadOnlyBuffer();
-        tmp.position(start);
-        tmp.limit(start + length);
-        return IntBufferOperation.lastIndexOf(tmp , is);
-    }
-
+    /**
+     * Only return a space which great than buffer.position() and less than
+     * buffer.limit()<br>
+     * <b>NOTE: It is will update the buffer</b>
+     * 
+     * @param buffer the {@link IntBuffer} which should be found out
+     * @return the data you want to
+     */
     public static int[] usedArray(IntBuffer buffer) {
         final int[] result = new int[buffer.limit() - buffer.position()];
         buffer.get(result);
