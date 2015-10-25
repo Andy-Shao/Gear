4bdc357797bd893b5ddec88b4ae6f8eb967ab3f5
Andy-Shao <hj13035952619@126.com>
2015-10-25 15:54:28 +0800

    add javadocs in DoubleBufferOperation

--- src/main/java/com/github/andyshao/nio/DoubleBufferOperation.java
+++ src/main/java/com/github/andyshao/nio/DoubleBufferOperation.java
@@ -13,6 +13,16 @@ import java.nio.DoubleBuffer;
  *
  */
 public final class DoubleBufferOperation {
+    /**
+     * Get data by position
+     * 
+     * @param buffer the {@link DoubleBuffer} which should be extracted
+     * @param start the start position of {@link DoubleBuffer} which part should
+     *            be extracted
+     * @param length the length of {@link DoubleBuffer} which is data that you
+     *            need it
+     * @return the data which you want to
+     */
     public static double[] getDoubles(DoubleBuffer buffer , int start , int length) {
         final DoubleBuffer tmp = buffer.asReadOnlyBuffer();
         tmp.position(start);
@@ -20,6 +30,16 @@ public final class DoubleBufferOperation {
         return DoubleBufferOperation.usedArray(tmp);
     }
 
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from head to end.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link DoubleBuffer} which should be found out from
+     * @param ds the data which should be found out for
+     * @return if can't find out then return -1
+     * @see DoubleBufferOperation#indexOf(DoubleBuffer, int, int, double...)
+     */
     public static int indexOf(DoubleBuffer buffer , double... ds) {
         if (ds.length == 0) throw new IllegalArgumentException("ds is empty");
         if (ds.length > buffer.limit() - buffer.position()) return -1;
@@ -43,6 +63,18 @@ public final class DoubleBufferOperation {
         return position;
     }
 
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from head to end.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link DoubleBuffer} which should be found out from
+     * @param start the start position of {@link DoubleBuffer}
+     * @param length the length of {@link DoubleBuffer} which part of it that
+     *            should be found out
+     * @param ds the data which should be found out for
+     * @return if can't find out then return -1
+     */
     public static int indexOf(DoubleBuffer buffer , int start , int length , double... ds) {
         DoubleBuffer tmp = buffer.asReadOnlyBuffer();
         tmp.position(start);
@@ -50,6 +82,16 @@ public final class DoubleBufferOperation {
         return DoubleBufferOperation.indexOf(tmp , ds);
     }
 
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from end to head.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link DoubleBuffer} which should be found out from
+     * @param ds the data which should be found out for
+     * @return if can't find out then return -1
+     * @see DoubleBufferOperation#lastIndexOf(DoubleBuffer, int, int, double...)
+     */
     public static int lastIndexOf(DoubleBuffer buffer , double... ds) {
         if (ds.length == 0) throw new IllegalArgumentException("ds is empty");
         if (ds.length > buffer.limit() - buffer.position()) return -1;
@@ -77,6 +119,17 @@ public final class DoubleBufferOperation {
         return position;
     }
 
+    /**
+     * Find the start point of data which you want to find out.
+     * The searching will start from end to head.<br>
+     * <b>NOTE: Only return the position of the first time find out the data</b>
+     * 
+     * @param buffer the {@link DoubleBuffer} which should be found out from
+     * @param start the start position of {@link DoubleBuffer}
+     * @param length the length of {@link DoubleBuffer} which should be searched
+     * @param ds the data which should be found out for
+     * @return if can't find out then return -1
+     */
     public static int lastIndexOf(DoubleBuffer buffer , int start , int length , double... ds) {
         DoubleBuffer tmp = buffer.asReadOnlyBuffer();
         tmp.position(start);
@@ -84,6 +137,14 @@ public final class DoubleBufferOperation {
         return DoubleBufferOperation.lastIndexOf(tmp , ds);
     }
 
+    /**
+     * Only return a space which great than buffer.position() and less than
+     * buffer.limit()<br>
+     * <b>NOTE: It is will update the buffer</b>
+     * 
+     * @param buffer the {@link DoubleBuffer} which should be extracted
+     * @return the data you want to
+     */
     public static double[] usedArray(DoubleBuffer buffer) {
         final double[] result = new double[buffer.limit() - buffer.position()];
         buffer.get(result);
