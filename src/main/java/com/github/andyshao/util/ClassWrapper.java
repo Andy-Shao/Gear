aff3e44347fd61289a56fa39eff4e8bdab1ac1ba
weichuang.shao <weichuang.shao@chinaredstar.com>
2017-01-02 14:39:13 +0800

    adding new logic coding

--- src/main/java/com/github/andyshao/util/ClassWrapper.java
+++ src/main/java/com/github/andyshao/util/ClassWrapper.java
@@ -1,39 +1,60 @@
 package com.github.andyshao.util;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import com.github.andyshao.reflect.FieldOperation;
 
 public final class ClassWrapper {
-	private ClassWrapper(){
-		throw new AssertionError("No " + ClassWrapper.class + " instance for you!");
-	}
-	
-	public static interface WrapperCovert{
-		Object covert(Object in, Class<?> inClazz, Class<?> outClazz);
-	}
-	
 	public static interface FieldMapper<IN, OUT> {
 		List<FieldMatch> match(IN in, OUT out);
 	}
 	
 	public static class FieldMatch{
-		private Field outField;
 		private Field inField;
+		private Field outField;
 		
 		public FieldMatch(Field inField, Field outField){
 			this.inField = inField;
 			this.outField = outField;
 		}
 		
-		public void setOutField(Field outField) {
-			this.outField = outField;
-		}
-		
 		public void setInField(Field inField) {
 			this.inField = inField;
 		}
+		
+		public void setOutField(Field outField) {
+			this.outField = outField;
+		}
+	}
+	
+	public static interface WrapperCovert{
+		Object covert(Object in, Class<?> inClazz, Class<?> outClazz);
+	}
+	
+	public static final <IN, OUT> void wrap(IN in, OUT out){
+		wrap(in, out, (input, output) -> {
+			if(input == null || output == null) return Collections.emptyList();
+			if(input.getClass().isPrimitive() || output.getClass().isPrimitive()) return Collections.emptyList();
+			List<FieldMatch> result = new ArrayList<>();
+			Field[] inputFields = FieldOperation.superGetDeclaredFields(input.getClass());
+			Field[] outputFields = FieldOperation.superGetDeclaredFields(output.getClass());
+			for(Field inputField : inputFields){
+				for(Field outputField : outputFields){
+					if(Objects.equals(inputField.getName(), outputField.getName())){
+						if(outputField.getClass().isAssignableFrom(inputField.getClass())) result.add(new FieldMatch(inputField, outputField));
+					}
+				}
+			}
+			return result;
+		});
+	}
+	
+	public static final <IN, OUT> void wrap(IN in, OUT out, FieldMapper<IN, OUT> fieldMapper){
+		wrap(in, out, fieldMapper, (input, inClazz, outClazz)->input);
 	}
 	
 	public static final <IN,OUT> void wrap(IN in, OUT out, FieldMapper<IN, OUT> fieldMapper, WrapperCovert covert){
@@ -46,8 +67,8 @@ public final class ClassWrapper {
 		}
 	}
 	
-	public static final <IN, OUT> void wrap(IN in, OUT out, FieldMapper<IN, OUT> fieldMapper){
-		wrap(in, out, fieldMapper, (input, inClazz, outClazz)->input);
+	private ClassWrapper(){
+		throw new AssertionError("No " + ClassWrapper.class + " instance for you!");
 	}
 
 }
