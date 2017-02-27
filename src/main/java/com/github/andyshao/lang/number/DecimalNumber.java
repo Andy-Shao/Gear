b9ee8d66622473d41193f792ac7ab8cd46a67d0a
Andy-Shao <hj13035952619@126.com>
2017-02-27 20:04:34 +0800

    updating

--- src/main/java/com/github/andyshao/lang/number/DecimalNumber.java
+++ src/main/java/com/github/andyshao/lang/number/DecimalNumber.java
@@ -4,6 +4,15 @@ import java.math.BigDecimal;
 
 import com.github.andyshao.lang.Convertable;
 
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 27 Feb 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
 public interface DecimalNumber extends Number , Convertable<BigDecimal> {
     default DecimalNumber add(DecimalNumber number) {
         return this.instance(this.convert().add(number.convert()));
