6521fb0bfb083fb15b5a5fd38309d57959207c19
Andy-Shao <hj13035952619@126.com>
2017-04-17 13:48:35 +0800

    add code

---
+++ src/main/java/com/github/andyshao/data/structure2/Queue.java
@@ -0,0 +1,26 @@
+package com.github.andyshao.data.structure2;
+
+import java.util.Iterator;
+
+/**
+ * 
+ * Title: Queue<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 17 Apr 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ * @param <Key>
+ */
+public class Queue<Key> implements Iterable<Key> {
+
+    @Override
+    public Iterator<Key> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void enqueue(Key key) {
+        
+    }
+}
