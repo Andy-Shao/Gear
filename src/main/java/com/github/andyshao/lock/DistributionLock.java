c1b08d77c5cbeec245aa84630188084fa3a5920d
Andy-Shao <hj13035952619@126.com>
2017-04-18 09:49:05 +0800

    add code

---
+++ src/main/java/com/github/andyshao/lock/DistributionLock.java
@@ -0,0 +1,14 @@
+package com.github.andyshao.lock;
+
+/**
+ * 
+ * Title: Distribution lock<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 18 Apr 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public interface DistributionLock {
+
+}
---
+++ src/main/java/com/github/andyshao/lock/ExpireMode.java
@@ -0,0 +1,14 @@
+package com.github.andyshao.lock;
+
+/**
+ * 
+ * Title: 延时模式<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) 18 Apr 2017<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public enum ExpireMode {
+    SECONDS,MILISECONDS,IGNORE;
+}
