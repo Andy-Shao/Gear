ef004436017ad524629ac428abb4c892632774da
unknown <weichuang.shao@zhaogang.int>
2018-03-28 14:44:12 +0800

    add MasterElectAlgorithm

---
+++ src/main/java/com/github/andyshao/election/MasterElectAlgorithm.java
@@ -0,0 +1,17 @@
+package com.github.andyshao.election;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) Mar 28, 2018<br>
+ * Encoding: UNIX UTF-8
+ * @author andy.shao
+ *
+ */
+public interface MasterElectAlgorithm {
+    Optional<ElectionNode> findMaster(List<ElectionNode> nodes);
+}
---
+++ src/test/java/com/github/andyshao/util/stream/CollectorImpleTest.java
@@ -0,0 +1,42 @@
+package com.github.andyshao.util.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class CollectorImpleTest {
+    
+    @Data
+    @AllArgsConstructor
+    class Person {
+        private String name;
+        private Integer age;
+    }
+
+    @Test
+    public void testToMap() {
+        List<Person> ls = Arrays.asList(new Person("andy" , 12), new Person("shao" , 45));
+        CollectorImpl<Person , List<Person> , Person> collectorImpl = new CollectorImpl<Person, List<Person>, Person>(
+                ArrayList::new, 
+                List::add, 
+                (left, right)-> {
+                    left.addAll(right);
+                    return left;
+                },
+                a -> a.get(0),
+                CollectorImpl.CH_CONCURRENT_NOID);
+        ConcurrentHashMap<String , Person> map = ls.stream().collect(Collectors.groupingBy(Person::getName , ConcurrentHashMap::new, collectorImpl));
+        Assert.assertNotNull(map);
+        Assert.assertThat(map.get("andy") , Matchers.is(new Person("andy", 12)));
+        Assert.assertThat(map.get("shao") , Matchers.is(new Person("shao" , 45)));
+    }
+}
