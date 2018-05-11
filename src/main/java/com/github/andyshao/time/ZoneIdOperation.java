ecdc1dab80af6390580098af47aeaa84a5fa3351
Andy-Shao <hj13035952619@126.com>
2018-05-11 17:23:42 +0800

    adding ZoneIdOperation

---
+++ src/main/java/com/github/andyshao/time/ZoneIdOperation.java
@@ -0,0 +1,16 @@
+package com.github.andyshao.time;
+
+/**
+ * 
+ * Title:<br>
+ * Descript:<br>
+ * Copyright: Copryright(c) May 11, 2018<br>
+ * Encoding:UNIX UTF-8
+ * @author Andy.Shao
+ *
+ */
+public final class ZoneIdOperation {
+    private ZoneIdOperation() {}
+    public static final String ASIA_BEIJING = "UTC+8";
+    public static final String CHINA = ASIA_BEIJING;
+}
