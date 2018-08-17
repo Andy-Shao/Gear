09d3d465b10a25a35d0c68079a263e840e86f410
Andy-Shao <hj1305952619@126.com>
2018-08-17 15:30:43 +0800

    fix bugs

--- src/main/java/com/github/andyshao/asm/Version.java
+++ src/main/java/com/github/andyshao/asm/Version.java
@@ -13,7 +13,8 @@ import org.objectweb.asm.Opcodes;
  *
  */
 public enum Version {
-    V1_1(Opcodes.V1_1) , V1_2(Opcodes.V1_2) , V1_3(Opcodes.V1_3) , V1_4(Opcodes.V1_4) , V1_5(Opcodes.V1_5) , V1_6(Opcodes.V1_6) , V1_7(Opcodes.V1_7) , V1_8(Opcodes.V1_8);
+    V1_1(Opcodes.V1_1) , V1_2(Opcodes.V1_2) , V1_3(Opcodes.V1_3) , V1_4(Opcodes.V1_4) , V1_5(Opcodes.V1_5) , 
+    V1_6(Opcodes.V1_6) , V1_7(Opcodes.V1_7) , V1_8(Opcodes.V1_8), V9(Opcodes.V9);
     private int version;
 
     private Version(int version) {
