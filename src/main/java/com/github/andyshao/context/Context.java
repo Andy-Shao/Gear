3a35b41c5fc19c5289d10e30ad71ecdd3071228e
Andy-Shao <hj13035952619@126.com>
2019-06-11 17:48:39 +0800

    commit unecessary implement

--- src/main/java/com/github/andyshao/context/Context.java
+++ src/main/java/com/github/andyshao/context/Context.java
@@ -1,7 +1,6 @@
 package com.github.andyshao.context;
 
 import java.util.concurrent.ConcurrentMap;
-import java.util.function.Function;
 
 /**
  * 
@@ -12,28 +11,4 @@ import java.util.function.Function;
  * @author Andy.Shao
  */
 public interface Context extends ConcurrentMap<String , Object>, CommonContext {
-    @SuppressWarnings("unchecked")
-    default <T> T setByType(ContextKey<T> key, T value) {
-        return (T) put(key.keyName() , value);
-    }
-    
-    @SuppressWarnings("unchecked")
-    default <T> T computeIfAbsent(ContextKey<T> key, Function<ContextKey<T> , T> function) {
-        return (T) computeIfAbsent(key.keyName() , k -> function.apply(key));
-    }
-    
-    default <T> boolean containsKey(ContextKey<T> key) {
-        return containsKey(key.keyName());
-    }
-    
-    @SuppressWarnings("unchecked")
-    default <T> T getByType(ContextKey<T> key) {
-        return (T) get(key.keyName());
-    }
-    
-    default <T> T getByTypeWithDefault(ContextKey<T> key, T defaultValue) {
-        T ret = getByType(key);
-        if(ret == null) return defaultValue;
-        return defaultValue;
-    }
 }
