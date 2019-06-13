53610bce72fc443bbd04520d5e67b93c65cf4de7
Andy-Shao <hj13035952619@126.com>
2019-06-13 17:22:39 +0800

    adjust projection

--- src/main/java/com/github/andyshao/util/CollectionOperation.java
+++ src/main/java/com/github/andyshao/util/CollectionOperation.java
@@ -1,16 +1,8 @@
 package com.github.andyshao.util;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
-import java.util.function.Supplier;
 
-import com.github.andyshao.data.structure.SimpleQueue;
 import com.github.andyshao.lang.ArrayWrapper;
-import com.github.andyshao.lang.Convert;
 
 /**
  * 
@@ -23,42 +15,6 @@ import com.github.andyshao.lang.Convert;
  *
  */
 public final class CollectionOperation {
-	public static final <E> Convert<Collection<E>, Queue<E>> toQueue() {
-		return toQueue(SimpleQueue::new);
-	}
-	
-	public static final <E> Convert<Collection<E>, Queue<E>> toQueue(Supplier<Queue<E>> supplier) {
-		return input -> {
-			Queue<E> queue = supplier.get();
-			queue.addAll(input);
-			return queue;
-		};
-	}
-	
-	public static final <E> Convert<Collection<E>, List<E>> toList() {
-		return toList(ArrayList::new);
-	}
-	
-	public static final <E> Convert<Collection<E>, List<E>> toList(Supplier<List<E>> supplier) {
-		return input -> {
-			List<E> list = supplier.get();
-			list.addAll(input);
-			return list;
-		};
-	}
-	
-	public static final <E> Convert<Collection<E>, Set<E>> toSet(Supplier<Set<E>> supplier) {
-		return input -> {
-			Set<E> set = supplier.get();
-			set.addAll(input);
-			return set;
-		};
-	}
-	
-	public static final <E> Convert<Collection<E>, Set<E>> toSet() {
-		return toSet(HashSet::new);
-	}
-	
     public static <E , ARRAY> void addAll(Collection<E> collection , ARRAY array) {
         CollectionOperation.addAll(collection , ArrayWrapper.wrap(array));
     }
