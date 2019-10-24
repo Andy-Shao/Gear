660bf6176382e1fb1a59e943ea95fb38c2e9c595
Andy-Shao <hj13035952619@126.com>
2019-10-24 17:13:24 +0800

    fix bugs

--- src/main/java/com/github/andyshao/util/stream/CollectorOperation.java
+++ src/main/java/com/github/andyshao/util/stream/CollectorOperation.java
@@ -1,13 +1,13 @@
 package com.github.andyshao.util.stream;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.github.andyshao.data.structure.SimpleQueue;
 import com.github.andyshao.lang.Convert;
@@ -25,15 +25,16 @@ import com.github.andyshao.lang.Convert;
 public final class CollectorOperation {
 	private CollectorOperation() {}
 	
-	public static <T, R> Collector<T, List<R>, List<R>> toList(Convert<T, R> covert) {
-		return CollectorImpl.<T, List<R>>idBuilder()
-				.withSupplier(ArrayList::new)
-				.withAccumulator((ls, it) -> ls.add(covert.convert(it)))
-				.withCombiner((l, r) -> {
-					l.addAll(r);
-					return l;
-				})
-				.build();
+	public static <T, R> Collector<T, ?, List<R>> toList(Convert<T, R> covert) {
+//		return CollectorImpl.<T, List<R>>idBuilder()
+//				.withSupplier(ArrayList::new)
+//				.withAccumulator((ls, it) -> ls.add(covert.convert(it)))
+//				.withCombiner((l, r) -> {
+//					l.addAll(r);
+//					return l;
+//				})
+//				.build();
+		return Collectors.mapping(it -> covert.convert(it), Collectors.toList());
 	}
 	
 	public static <T, R> Collector<T, Queue<R>, List<R>> toListConcurrent(Convert<T, R> convert) {
@@ -50,15 +51,16 @@ public final class CollectorOperation {
 				.build();
 	}
 	
-	public static <T, R> Collector<T, Set<R>, Set<R>> toSet(Convert<T, R> convert) {
-		return CollectorImpl.<T, Set<R>>idBuilder()
-				.withSupplier(HashSet::new)
-				.withAccumulator((set, it) -> set.add(convert.convert(it)))
-				.withCombiner((l, r) -> {
-					l.addAll(r);
-					return l;
-				})
-				.build();
+	public static <T, R> Collector<T, ?, Set<R>> toSet(Convert<T, R> convert) {
+//		return CollectorImpl.<T, Set<R>>idBuilder()
+//				.withSupplier(HashSet::new)
+//				.withAccumulator((set, it) -> set.add(convert.convert(it)))
+//				.withCombiner((l, r) -> {
+//					l.addAll(r);
+//					return l;
+//				})
+//				.build();
+		return Collectors.mapping(it -> convert.convert(it), Collectors.toSet());
 	}
 	
 	public static <T, R> Collector<T, ConcurrentSkipListSet<R>, ConcurrentSkipListSet<R>> toSetConcurrent(
