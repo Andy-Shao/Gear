9750f5ca8018dca219d029cd8a13b291977df7b2
Andy-Shao <hj13035952619@126.com>
2019-10-24 14:25:59 +0800

    adding junit test case

--- src/test/java/com/github/andyshao/util/stream/CollectorOperationTest.java
+++ src/test/java/com/github/andyshao/util/stream/CollectorOperationTest.java
@@ -3,7 +3,10 @@ package com.github.andyshao.util.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.stream.Collectors;
 
 import org.assertj.core.api.Assertions;
@@ -29,6 +32,46 @@ class CollectorOperationTest {
 		Assertions.assertThat(map).isNotNull();
 		Assertions.assertThat(map).isNotEmpty();
 		Assertions.assertThat(map.get("andy.shao")).isEqualTo(Arrays.asList(29, 30));
+	}
+	
+	@Test
+	void toSet() {
+		List<Person> ps = Arrays.<Person>asList(new Person("andy.shao", 29), new Person("andy.shao", 30));
+		Map<String, Set<Integer>> map = ps.stream()
+			.collect(Collectors.groupingBy(Person::getName, CollectorOperation.toSet(Person::getAge)));
+		Assertions.assertThat(map).isNotNull();
+		Assertions.assertThat(map.size()).isEqualTo(1);
+		Assertions.assertThat(map.containsKey("andy.shao")).isTrue();
+	}
+	
+	@Test
+	void toQueue() {
+		List<Person> ps = Arrays.<Person>asList(new Person("andy.shao", 29), new Person("andy.shao", 30));
+		Map<String, Queue<Integer>> map = ps.stream()
+			.collect(Collectors.groupingBy(Person::getName, CollectorOperation.toQueue(Person::getAge)));
+		Assertions.assertThat(map).isNotNull();
+		Assertions.assertThat(map.size()).isEqualTo(1);
+		Assertions.assertThat(map.get("andy.shao").toArray(new Integer[2])).isEqualTo(new Integer[] {29, 30});
+	}
+	
+	@Test
+	void toQueueConcurrent() {
+		List<Person> ps = Arrays.<Person>asList(new Person("andy.shao", 29), new Person("andy.shao", 30));
+		Map<String, Queue<Integer>> map = ps.stream()
+				.collect(Collectors.groupingBy(Person::getName, CollectorOperation.toQueueConcurrent(Person::getAge)));
+		Assertions.assertThat(map).isNotNull();
+		Assertions.assertThat(map.size()).isEqualTo(1);
+		Assertions.assertThat(map.get("andy.shao").toArray(new Integer[2])).isEqualTo(new Integer[] {29, 30});
+	}
+	
+	@Test
+	void toSetConcurrent() {
+		List<Person> ps = Arrays.<Person>asList(new Person("andy.shao", 29), new Person("andy.shao", 30));
+		Map<String, ConcurrentSkipListSet<Integer>> map = ps.parallelStream()
+				.collect(Collectors.groupingBy(Person::getName, CollectorOperation.toSetConcurrent(Person::getAge)));
+		Assertions.assertThat(map).isNotNull();
+		Assertions.assertThat(map.size()).isEqualTo(1);
+		Assertions.assertThat(map.containsKey("andy.shao")).isTrue();
 	}
 	
 	@Test
